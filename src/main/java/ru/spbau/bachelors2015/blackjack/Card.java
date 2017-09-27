4c61734cc0b361a5b9af18a732c58fde78479daf
Ivan Veselov <veselov.iva@gmail.com>
2017-09-27 15:15:42 +0300

    Add Game interface.

---
+++ src/main/java/ru/spbau/bachelors2015/blackjack/Card.java
@@ -0,0 +1,7 @@
+package ru.spbau.bachelors2015.blackjack;
+
+public class Card {
+    public int value() {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/spbau/bachelors2015/blackjack/Game.java
@@ -0,0 +1,19 @@
+package ru.spbau.bachelors2015.blackjack;
+
+public class Game {
+    public Card nextCard() {
+        return null;
+    }
+
+    public void pass() {
+
+    }
+
+    public int playerPoints() {
+        return 0;
+    }
+
+    public int computerPoints() {
+        return 0;
+    }
+}
