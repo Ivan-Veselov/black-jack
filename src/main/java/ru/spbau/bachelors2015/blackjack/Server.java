2384e72852667df768b76374b4cc0ff50ea446f1
Ilya <scorpioni4@yandex.ru>
2017-11-08 15:18:37 +0300

    Server interface added

---
+++ src/main/java/ru/spbau/bachelors2015/blackjack/Server.java
@@ -0,0 +1,14 @@
+package ru.spbau.bachelors2015.blackjack;
+
+public interface Server {
+    enum Status {YOUR_TURN, WAITING, WIN, LOSE}
+
+    void connect();
+    void disconnect();
+
+    boolean isStarted();
+    Status status();
+
+    void pass();
+    Card nextCard();
+}
