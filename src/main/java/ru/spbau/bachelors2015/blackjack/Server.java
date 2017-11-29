bcc582f580b65c120c490fb11f44c909f623f89b
Ivan Veselov <veselov.iva@gmail.com>
2017-11-29 14:47:52 +0300

    Implement ServerCommunicator methods.

--- src/main/java/ru/spbau/bachelors2015/blackjack/Server.java
+++ src/main/java/ru/spbau/bachelors2015/blackjack/Server.java
@@ -1,10 +1,12 @@
 package ru.spbau.bachelors2015.blackjack;
 
+import java.io.IOException;
+
 public interface Server {
     enum Status {YOUR_TURN, WAITING, WIN, LOSE}
 
-    void connect();
-    void disconnect();
+    void connect() throws IOException;
+    void disconnect() throws IOException;
 
     boolean isStarted();
     Status status();
